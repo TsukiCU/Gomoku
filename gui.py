2f07285afee1b40c0d25bc364a3c95d8517d58be
TsukiCU <hs3475@columbia.edu>
2024-02-23 16:31:13 -0500

    simple gui. nd to change the bg.jpg. too small rn.

---
+++ gui.py
@@ -0,0 +1,79 @@
+import pygame
+import os
+import sys
+
+EMPTY = 0
+BLACK = 1
+WHITE = 2
+BLACK_COLOR = [0, 0, 0]
+WHITE_COLOR = [255, 255, 255]
+BOARD_SIZE = 15
+BORDER_WIDTH = 1
+GRID_SIZE = 40
+
+background_img = pygame.image.load(os.path.join('assets', 'background.jpg'))
+
+class RenjuBoard:
+    def __init__(self):
+        self._board = [[EMPTY] * BOARD_SIZE for _ in range(BOARD_SIZE)]
+
+    def reset(self):
+        for row in range(BOARD_SIZE):
+            self._board[row] = [EMPTY] * BOARD_SIZE
+
+    def move(self, row, col, is_black):
+        if self._board[row][col] == EMPTY:
+            self._board[row][col] = BLACK if is_black else WHITE
+            return True
+        return False
+
+    def draw(self, screen):
+        for h in range(1, BOARD_SIZE + 1):
+            pygame.draw.line(screen, BLACK_COLOR, [GRID_SIZE, h * GRID_SIZE], [BOARD_SIZE * GRID_SIZE, h * GRID_SIZE], 1)
+            pygame.draw.line(screen, BLACK_COLOR, [h * GRID_SIZE, GRID_SIZE], [h * GRID_SIZE, BOARD_SIZE * GRID_SIZE], 1)
+
+        pygame.draw.rect(screen, BLACK_COLOR, [GRID_SIZE - BORDER_WIDTH, GRID_SIZE - BORDER_WIDTH, (BOARD_SIZE + 1) * GRID_SIZE, (BOARD_SIZE + 1) * GRID_SIZE], BORDER_WIDTH)
+
+        pygame.draw.circle(screen, BLACK_COLOR, [GRID_SIZE * 8, GRID_SIZE * 8], 5, 0)  # Central
+        for x in [GRID_SIZE * 4, GRID_SIZE * 12]:
+            for y in [GRID_SIZE * 4, GRID_SIZE * 12]:
+                pygame.draw.circle(screen, BLACK_COLOR, [x, y], 3, 0)
+
+        for row in range(BOARD_SIZE):
+            for col in range(BOARD_SIZE):
+                if self._board[row][col] != EMPTY:
+                    color = BLACK_COLOR if self._board[row][col] == BLACK else WHITE_COLOR
+                    pos = [GRID_SIZE * (col + 1), GRID_SIZE * (row + 1)]
+                    pygame.draw.circle(screen, color, pos, 18, 0)
+
+
+def main():
+    pygame.init()
+    size = width, height = (BOARD_SIZE + 1) * GRID_SIZE, (BOARD_SIZE + 1) * GRID_SIZE
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Gomoku")
+
+    board = RenjuBoard()
+    clock = pygame.time.Clock()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill([255, 255, 255])
+        screen.blit(background_img, (0, 0))
+        board.draw(screen)
+        pygame.display.flip()
+
+        clock.tick(60)
+
+    pygame.quit()
+    sys.exit()
+
+if __name__ == '__main__':
+    main()
+
+
+
