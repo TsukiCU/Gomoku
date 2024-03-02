2913e6dbbec65bc6c6f932d24c203c7ec893fa5d
TsukiCU <hs3475@columbia.edu>
2024-03-02 17:05:59 -0500

    add some functions

--- src/gomokuAI.cpp
+++ src/gomokuAI.cpp
@@ -1,1 +1,49 @@
 #include "gomokuAI.h"
+
+vector<pair<int, int>> GomokuAI::getLegalMoves()
+{
+    vector<pair<int, int>> legalMoves;
+    for (int row=0; row<game->board_size; row++)
+        for (int col=0; col<game->board_size; col++)
+            if (!game->board[row][col])
+                legalMoves.push_back(make_pair(row, col));
+
+    return legalMoves;
+}
+
+vector<pair<int, int>> GomokuAI::getLegalMoves(int heuristic)
+{
+    /* TODO: Heuristic searching.
+     * Probably no need to store every possible move on the board.
+     */
+    vector<pair<int, int>> legalMoves;
+    return legalMoves;
+}
+
+int GomokuAI::evaluate(Gomoku &game)
+{
+    return 0;
+}
+
+int GomokuAI::evaluate(Gomoku &game, int heuristic)
+{
+    /* TODO: Heuristic evaluation. */
+    return 0;
+}
+
+int GomokuAI::makeMove(int x, int y)
+{
+    if (!game->make_move(x, y)) {
+        cout << "AI makes move at " << x << ", " << y << endl;
+        return 0;
+    }
+    else {
+        cout << "Invalid move at " << x << ", " << y << endl;
+        return 1;
+    }
+}
+
+int GomokuAI::MinMax(Gomoku &game, int depth, int alpha, int beta)
+{
+    return 0;
+}
--- src/gomokuAI.h
+++ src/gomokuAI.h
@@ -2,16 +2,25 @@
 #define __GOMOKUAI_HH
 
 #include "gomoku.h"
+#include <climits>
+#include <queue>
 
 class GomokuAI {
 private:
-    Gomoku *game;
+    Gomoku *game;                                               // AI holds reference to the game instance.
 
 public:
+    bool maiximizingPlayer;                                     // Is it maximizing or minimizing?
+    int  maxDepth;                                              // Max calculating depths.
     GomokuAI(Gomoku *game) : game(game) {}
 
-    int evaluate(vector<vector<int>> &board);                   // Evaluation of the current board.
-    int evaluate(vector<vector<int>> &board, int heuristic);    // TODO: heuristic evaluation method.
+    vector<pair<int, int>> getLegalMoves();                     // Get valid intersections on board.
+    vector<pair<int, int>> getLegalMoves(int heuristic);        // Focus on the possible areas to reduce overhead.
+    int evaluate(Gomoku &game);                                 // Evaluation of the current board.
+    int evaluate(Gomoku &game, int heuristic);                  // Heuristic evaluation for optimizing.
+    int makeMove(int x, int y);                                 // AI makes a move at (x, y).
+    int undoMove(int x, int y);                                 // Undo a move at (x, y). Used when searching.
+    int MinMax(Gomoku &game, int depth, int alpha, int beta);   // Alpha Beta Prunning.
 };
 
 #endif
